0570234d9cfe63b6440f07fe082eac2056252f88
Andy Shea <andrew@octahedron.com.au>
2016-10-04 14:31:13 +0100

    Refactored publish flow to allow modules

--- test.js
+++
@@ -1,6 +0,0 @@
-import render from './src/client';
-import load from './src/server/load';
-import HttpError from './src/server/HttpError';
-
-const err = new HttpError(404, 'not found');
-console.log(err.notFound, HttpError.notFound());
