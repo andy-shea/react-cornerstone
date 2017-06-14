08a2fa8e6c05ec9569fcf98a19e7c6f9e91b982c
Andy Shea <andrew@octahedron.com.au>
2017-06-14 15:12:52 +0100

    Provision of middleware now requires function to allow request to be passed in on server-side

--- src/common/store.js
+++ src/common/store.js
@@ -3,21 +3,21 @@ import {routerReducer, routerMiddleware} from 'react-router-redux';
 import {reducer as reduxAsyncConnect} from 'redux-connect';
 import thunkMiddleware from 'redux-thunk';
 
-function configureStoreCreator(reducers, middleware = [thunkMiddleware]) {
+function configureStoreCreator(reducers, middleware = () => [thunkMiddleware]) {
   const reducer = combineReducers({
     routing: routerReducer,
     reduxAsyncConnect,
     ...reducers
   });
 
-  return (forClient, history, initialState = {}) => {
+  return (forClient, history, initialState = {}, req) => {
     const devToolsEnhancer = forClient && window.devToolsExtension ? window.devToolsExtension() : f => f;
     return createStore(
       reducer,
       initialState,
       compose(
         applyMiddleware(
-          ...middleware,
+          ...middleware(req),
           routerMiddleware(history)
         ),
         devToolsEnhancer
