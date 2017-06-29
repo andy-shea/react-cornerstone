3331f34d193317b3440a497566f79cdce06b3034
Andy Shea <andrew@octahedron.com.au>
2017-06-29 13:20:42 +0100

    Add support for router middleware

--- src/client/render.jsx
+++ src/client/render.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import {render as renderToDom} from 'react-dom';
 import {Provider} from 'react-redux';
 import {ReduxAsyncConnect} from 'redux-connect';
-import {Router, browserHistory} from 'react-router';
+import {Router, browserHistory, applyRouterMiddleware} from 'react-router';
 import {syncHistoryWithStore} from 'react-router-redux';
 
-function render(configureStore, createRoutes, mountTo, helpers = {}) {
+function render(configureStore, createRoutes, mountTo, {middleware, helpers}) {
   const store = configureStore(true, browserHistory, window.__INITIAL_STATE__);
   const history = syncHistoryWithStore(browserHistory, store);
 
   renderToDom(
     <Provider store={store}>
-      <Router render={props => <ReduxAsyncConnect helpers={helpers} {...props}/>} history={history} routes={createRoutes(store)}/>
+      <Router history={history} routes={createRoutes(store)} render={props => (
+        <ReduxAsyncConnect helpers={helpers || {}} {...props} render={applyRouterMiddleware(...(middleware || []))}/>
+      )}/>
     </Provider>,
     mountTo
   );
