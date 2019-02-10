1ad8299f2c9a658f202f52a1923dc5479ca2e58f
Andy Shea <andy@tjs.com>
2019-02-10 12:23:07 +0000

    Dep update

--- src/common/store.js
+++ src/common/store.js
@@ -4,9 +4,15 @@ import thunkMiddleware from 'redux-thunk';
 
 function configureStoreCreator(reducers, middleware = () => [thunkMiddleware]) {
   return (forClient, {map, ...routesConfig}, initialState = {}, req) => {
-    const {reducer, middleware: routerMiddleware, enhancer, thunk} = connectRoutes(map, routesConfig);
+    const {reducer, middleware: routerMiddleware, enhancer, thunk} = connectRoutes(
+      map,
+      routesConfig
+    );
     const rootReducer = combineReducers({location: reducer, ...reducers});
-    const devToolsEnhancer = forClient && window.devToolsExtension ? window.devToolsExtension() : f => f;
+    const devToolsEnhancer =
+      forClient && window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : f => f;
     const store = createStore(
       rootReducer,
       initialState,
